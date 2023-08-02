40eb7d967126d8eb3d02ea64aefb482f38593cf6
PedroSavio <saviokbca@gmail.com>
2023-08-01 21:25:08 -0300

    services and config spring jwt security

--- src/main/java/com/midia/loginSecurity/user/config/JwtAuthenticationFilter.java
+++ src/main/java/com/midia/loginSecurity/user/config/JwtAuthenticationFilter.java
@@ -58,5 +58,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        filterChain.doFilter(request, response);
     }
 }
