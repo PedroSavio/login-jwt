98387579d189aa95b9c1f42ed6976d56edef3356
PedroSavio <saviokbca@gmail.com>
2023-08-02 20:42:11 -0300

    fix authHeader

--- src/main/java/com/midia/loginSecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/midia/loginSecurity/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,15 +27,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            @NotNull HttpServletRequest request,
-            @NotNull HttpServletResponse response,
-            @NotNull FilterChain filterChain
+            @NonNull  HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         // Pega o token do header JWT
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+        if(authHeader == null || authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
